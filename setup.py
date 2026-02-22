d08dde81d58427ed45e188bd464ddbc6d7e438db
sarthak <sarthakrawat1092@gmail.com>
2026-02-22 10:47:00 +0530

    updated notebook

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="medical_chatbot",
     version="0.1.0",
-    author="Sarthak Singh Rawat",
+    author="Sarthak Singh Rawat and Tansihk Bhatnagar",
     author_email="sarthakrawat1092@gmail.com",
     packages=find_packages(),
     install_requires=[
